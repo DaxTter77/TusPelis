eb3893c73395c18e6c4805326371460a5b6015a6
William-A-Ballesteros <william.al.ballesteros.cortes@gmail.com>
2024-11-08 22:32:51 -0500

    Login working

--- client-tuspelis/src/constants/global.js
+++ client-tuspelis/src/constants/global.js
@@ -2,3 +2,8 @@
 const baseUrl = "http://127.0.0.1:8000/api/v1/";
 
 export { baseUrl };
+
+// Token
+
+export const ACCESS_TOKEN = "access";
+export const REFRESH_TOKEN = "refresh";
---
+++ client-tuspelis/src/pages/NoFound.jsx
@@ -0,0 +1,6 @@
+export function NoFound(){
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>La págima que consulta no existe</p>
+    </div>
+}
