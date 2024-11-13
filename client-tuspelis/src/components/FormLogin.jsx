40311898cf0290bc600022bea67f5842c97362c8
William-A-Ballesteros <william.al.ballesteros.cortes@gmail.com>
2024-11-13 17:23:19 -0500

    se cambia la imagen de login

--- client-tuspelis/src/components/FormLogin.jsx
+++ client-tuspelis/src/components/FormLogin.jsx
@@ -37,7 +37,7 @@ export function FormLogin({router, method}){
     return (<div className="min-h-screen flex items-center justify-center bg-sky-100">
   <div className="bg-sky-100 flex justify-center items-center h-screen w-full">
     <div className="w-1/2 h-screen hidden lg:block">
-      <img src="https://img.freepik.com/fotos-premium/imagen-fondo_910766-187.jpg?w=826" alt="Placeholder Image" className="object-cover w-full h-full" />
+      <img src="https://i.imgur.com/5JaVvmo.png" alt="Placeholder Image" className="object-cover w-full h-full" />
     </div>
     <div className="lg:p-36 md:p-52 sm:p-20 p-8 w-full lg:w-1/2 text-gray-950">
       <h1 className="text-2xl font-semibold mb-4">Log into MyMovies</h1>
