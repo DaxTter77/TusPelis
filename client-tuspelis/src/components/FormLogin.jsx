978beb66714038f1fd8135b7892f73332493501d
William-A-Ballesteros <william.al.ballesteros.cortes@gmail.com>
2024-11-14 16:03:53 -0500

    Se arregla error en el login por falta de valores y onchange en el formulario de login

--- client-tuspelis/src/components/FormLogin.jsx
+++ client-tuspelis/src/components/FormLogin.jsx
@@ -37,18 +37,18 @@ export function FormLogin({router, method}){
     return (<div className="min-h-screen flex items-center justify-center bg-sky-100">
   <div className="bg-sky-100 flex justify-center items-center h-screen w-full">
     <div className="w-1/2 h-screen hidden lg:block">
-      <img src="https://i.imgur.com/5JaVvmo.png" alt="Placeholder Image" className="object-cover w-full h-full" />
+      <img src="https://i.imgur.com/5JaVvmo.png" alt="Placeholder" className="object-cover w-full h-full" />
     </div>
     <div className="lg:p-36 md:p-52 sm:p-20 p-8 w-full lg:w-1/2 text-gray-950">
       <h1 className="text-2xl font-semibold mb-4">Log into MyMovies</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-4 bg-sky-100">
           <label htmlFor="username" className="block text-gray-600">Username</label>
-          <input type="email" id="email" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 text-gray-950" autoComplete="off" />
+          <input type="email" id="email" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 text-gray-950" autoComplete="off" value={email}  onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="mb-4">
           <label htmlFor="password" className="block text-gray-800">Password</label>
-          <input type="password" id="password" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 text-gray-950" autoComplete="off" />
+          <input type="password" id="password" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 text-gray-950" autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
         <div className="mb-4 flex items-center">
           <input type="checkbox" id="remember" name="remember" className="text-red-500" />
