62ee3bcd57c34c2a9b1c97a90ed95c14a8600585
weisman713 <weisman2015@gmail.com>
2024-11-14 23:37:54 -0500

    se estiliza el favoritos el tipo favoritos y la navegacion

--- client-tuspelis/src/pages/TipoFavoritoPagina.jsx
+++ client-tuspelis/src/pages/TipoFavoritoPagina.jsx
@@ -1,13 +1,22 @@
-import { Link } from "react-router-dom"
-import { TiposFavoritosList } from "../components/TipoFavoritoLista"
+import { Link } from "react-router-dom";
+import { TiposFavoritosList } from "../components/TipoFavoritoLista";
 
-export function TipoFavoritoPagina(){
+export function TipoFavoritoPagina() {
     return (
-        <div className="container">
-            <h1>Tipos de Favoritos</h1>
-            <Link to="/tipos-favoritos/add">Agregar Tipo Favorito</Link>
-            <br />
+        <div className="container mx-auto px-4 py-8">
+            
+            <div className="flex justify-between mb-8">
+            <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-200 mb-6 text-center">
+                Tipos de Favoritos
+            </h1>
+                <Link 
+                    to="/tipos-favoritos/add"
+                    className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 dark:bg-indigo-500 dark:hover:bg-indigo-600 transition-colors duration-150 shadow-md"
+                >
+                    Agregar Tipo Favorito
+                </Link>
+            </div>
             <TiposFavoritosList />
         </div>
-    )
+    );
 }
