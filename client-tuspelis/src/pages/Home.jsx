1a20f0fb39bd2194182bec71d36748005283cecb
DYLANPC\DR <dylrc12@gmail.com>
2024-11-14 20:59:00 -0500

    Se implementa el llamado para traer información del usuario

--- client-tuspelis/src/pages/Home.jsx
+++ client-tuspelis/src/pages/Home.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
+import { authApi } from '../api/auth.api';
+
 export function Home(){
+    const [nombre, setNombre] = useState("None"); 
+    const [apellido, setApellido] = useState("None"); 
+    const [username, setUsername] = useState(""); 
+    
+    useEffect(() => {
+        const loadData = async () =>{
+            const res = await authApi.get("/me");
+            console.log(res);
+            if(res.data){
+                setNombre(res.data.first_name ? res.data.first_name : "");
+                setApellido(res.data.last_name ? res.data.last_name : "");
+                setUsername(res.data.username ? res.data.username : "");
+            }
+        }
+        loadData();
+    })
     return <div>
         <h1 className="underline decoration-1">Inicio</h1>
         <p>Bienvenido a tusPelis</p>
+        <p>Usuario: {username}</p>
+        {
+            nombre && <p>{nombre} {apellido}</p>
+        }
     </div>
 }
