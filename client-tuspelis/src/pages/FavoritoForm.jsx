4ca94f5855efa63f02cf8d3f328bd58fa198868a
DYLANPC\DR <dylrc12@gmail.com>
2024-11-17 13:44:27 -0500

    Se ajustan los estilos del campo

--- client-tuspelis/src/pages/FavoritoForm.jsx
+++ client-tuspelis/src/pages/FavoritoForm.jsx
@@ -94,7 +94,7 @@ export function FavoritoForm(){
                     <input type="text" placeholder="Ingrese el genero" name="txtGenero" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer col-start-3 row-start-2" id="txtGenero" {...register("genero", { required : true })} />
                     {errors.genero && <span style={{color:"red"}}>El genero es requerido</span>}
                     <br />
-                    <input 
+                    <input className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer col-start-3 row-start-2"
                         type="file" 
                         accept="image/*"
                         {...register('imagen_caratula')}
